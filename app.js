71fab023671d08c5b688134e5033ae78e670c53d
adaao <armascarelli@gmail.com>
2020-07-21 08:40:02 -0300

    Rota currículo

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//rotas
 app.get('/', function(req, res, next){
    res.render('index',{
       title: 'meu servido em Express',
@@ -16,6 +17,32 @@ app.get('/', function(req, res, next){
    });
 });
 
+app.get('/curriculo', function(req, res, next){
+   res.render('curriculo', {
+      title: 'Meu currículo',
+      name: 'Adaão R. Mascarelli',
+      profession: 'Autônomo',
+      description: 'Estudando desenvolvimento com JavaScript, realizo trabalhos avulsos de manutençaõ e suporte de micro computadores, e entregas de bicicleta por aplicativos.',
+      experience: [{
+          company: 'World Computer',
+          office: 'Estagiário',
+          description: 'Prestar suporte N1, manutenção de computadores, automação de processos com scripts (Python, Shell, CMD).'
+      },
+      {
+          company: 'Saybolt Inspeções Técnicas',
+          office: 'Inspetor 1',
+          description: 'Inspecionar navios e moegas, realizar amostragem, documentar observações.'
+      }],
+      education: [{
+          institution: 'FATEC Rubens Lara',
+          description: 'Análise e Desenvolvimento de Sistemas.'
+      }],
+      skills: ['Shell', 'JavaScript', 'Docker']
+  });
+});
+
+
+//server
 app.listen(port, err => {
    console.log(`Server listening on port ${port}`);
 })
