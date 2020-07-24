97a42b3b45a3b2d0aaf4968ca71d3bd592288b59
adaao <armascarelli@gmail.com>
2020-07-24 09:13:29 -0300

    app.js actualization

--- app.js
+++ app.js
@@ -1,46 +1,20 @@
 const express = require('express');
+const CurriculoController = require('./controllers/curriculo-controller');
 const path = require('path');
 
 const port = 3000;
 const app = express();
 
+const indexRoute = require('./routes/index');
+const curriculoRoutes = require('./routes/curriculo-routes');
+
+app.get('/', indexRoute);
+app.get('/curriculo', curriculoRoutes);
+
 
 //setup view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-//rotas
-app.get('/', function(req, res, next){
-   res.render('index',{
-      title: 'meu servido em Express',
-      version: "0.0.0"
-   });
-});
-
-app.get('/curriculo', function(req, res, next){
-   res.render('curriculo', {
-      title: 'Meu currículo',
-      name: 'Adaão R. Mascarelli',
-      profession: 'Autônomo',
-      description: 'Estudando desenvolvimento com JavaScript, realizo trabalhos avulsos de manutençaõ e suporte de micro computadores, e entregas de bicicleta por aplicativos.',
-      experience: [{
-          company: 'World Computer',
-          office: 'Estagiário',
-          description: 'Prestar suporte N1, manutenção de computadores, automação de processos com scripts (Python, Shell, CMD).'
-      },
-      {
-          company: 'Saybolt Inspeções Técnicas',
-          office: 'Inspetor 1',
-          description: 'Inspecionar navios e moegas, realizar amostragem, documentar observações.'
-      }],
-      education: [{
-          institution: 'FATEC Rubens Lara',
-          description: 'Análise e Desenvolvimento de Sistemas.'
-      }],
-      skills: ['Shell', 'JavaScript', 'Docker']
-  });
-});
-
 
 //server
 app.listen(port, err => {
