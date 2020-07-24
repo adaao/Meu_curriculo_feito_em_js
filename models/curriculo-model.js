690d243b5b1d20de0bf9bd8e112318ffb42b18a9
adaao <armascarelli@gmail.com>
2020-07-24 08:33:46 -0300

    Curriculo model

---
+++ models/curriculo-model.js
@@ -0,0 +1,23 @@
+const data = {
+   title: 'Meu currículo',
+   name: 'Adaão R. Mascarelli',
+   profession: 'Autônomo',
+   description: 'Estudando desenvolvimento com JavaScript, realizo trabalhos avulsos de manutençaõ e suporte de micro computadores, e entregas de bicicleta por aplicativos.',
+   experience: [{
+       company: 'World Computer',
+       office: 'Estagiário',
+       description: 'Prestar suporte N1, manutenção de computadores, automação de processos com scripts (Python, Shell, CMD).'
+   },
+   {
+       company: 'Saybolt Inspeções Técnicas',
+       office: 'Inspetor 1',
+       description: 'Inspecionar navios e moegas, realizar amostragem, documentar observações.'
+   }],
+   education: [{
+       institution: 'FATEC Rubens Lara',
+       description: 'Análise e Desenvolvimento de Sistemas.'
+   }],
+   skills: ['Shell', 'JavaScript', 'Docker']
+}
+
+module.exports = data;
