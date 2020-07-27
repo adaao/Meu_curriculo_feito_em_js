344b5e89708d0ddf55a71c3a0c8d2764124013b5
adaao <armascarelli@gmail.com>
2020-07-27 07:38:27 -0300

    Tratamento de erros

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const CurriculoController = require('./controllers/curriculo-controller');
 const path = require('path');
+const createError = require('http-errors');
 
 const port = 3000;
 const app = express();
@@ -11,6 +12,21 @@ const curriculoRoutes = require('./routes/curriculo-routes');
 
 app.get('/', indexRoute);
 app.get('/curriculo', curriculoRoutes);
+
+//Errors
+//404
+app.use((req, res, next) => {
+   next(createError(404));
+});
+
+//error handler
+app.use((err, req, res, next) => {
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+   res.status(err.status || 500);
+   res.render('error');
+})
 
 
 //setup view engine
